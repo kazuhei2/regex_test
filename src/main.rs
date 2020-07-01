f0612ffb1545dc0dc95ed171f757c9ab2adf0555
Kazuhei Hamada <hamada_kazuhei@cosmic-eng.co.jp>
2020-07-01 11:26:38 +0900

    Refactor ip a command output parsing

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,15 @@ use std::str;
 use regex::Regex;
 
 fn main() {
-    let re_mac = Regex::new(r"link/\w+ ([[:xdigit:]]{2}):([[:xdigit:]]{2}):([[:xdigit:]]{2}):([[:xdigit:]]{2}):([[:xdigit:]]{2}):([[:xdigit:]]{2})").unwrap();
-
-    let output = Command::new("ip").arg("a").output().expect("failed to ip command");
+    let output = Command::new("ip").arg("a").output().expect("failed at ip command");
     let text = str::from_utf8(&output.stdout).unwrap();
-
+    let re_mac = Regex::new(r"link/\w+ (?P<mac>[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2})").unwrap();
     for caps in re_mac.captures_iter(text) {
-        println!("mac is {}:{}:{}:{}:{}:{}",
-                 caps.get(1).unwrap().as_str(),
-                 caps.get(2).unwrap().as_str(),
-                 caps.get(3).unwrap().as_str(),
-                 caps.get(4).unwrap().as_str(),
-                 caps.get(5).unwrap().as_str(),
-                 caps.get(6).unwrap().as_str());
+        println!("mac is {}", &caps["mac"]);
     }
-    let re_ip_cidr = Regex::new(r"inet (\d{1,3}).(\d{1,3}).(\d{1,3}).(\d{1,3})/(\d{1,2})").unwrap();
+    let re_ip_cidr = Regex::new(r"inet (?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})/(?P<cidr>\d{1,2})").unwrap();
     for caps in re_ip_cidr.captures_iter(text) {
-        println!("ipv4 address and cidr is {}.{}.{}.{}/{}",
-                 caps.get(1).unwrap().as_str(),
-                 caps.get(2).unwrap().as_str(),
-                 caps.get(3).unwrap().as_str(),
-                 caps.get(4).unwrap().as_str(),
-                 caps.get(5).unwrap().as_str());
+        println!("ipv4 address and cidr is {}/{}", &caps["ip"], &caps["cidr"]);
     }
 
 
