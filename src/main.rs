08aee2b1832d8b99484128fc45886840f7257058
Kazuhei Hamada <hamada_kazuhei@cosmic-eng.co.jp>
2020-07-01 11:27:01 +0900

    Add ifconfig output parsing program

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 use std::str;
+use std::net::Ipv4Addr;
 
 use regex::Regex;
 
@@ -15,5 +16,18 @@ fn main() {
         println!("ipv4 address and cidr is {}/{}", &caps["ip"], &caps["cidr"]);
     }
 
-
+    let output = Command::new("ifconfig").output().expect("failed at ifconfig command");
+    let text = str::from_utf8(&output.stdout).unwrap();
+    let re_ip_netmask = Regex::new(r"inet (?P<ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})  netmask (?P<netmask>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})").unwrap();
+    for caps in re_ip_netmask.captures_iter(text) {
+        let ip: Ipv4Addr = caps["ip"].parse().unwrap();
+        if ip.is_loopback() {
+            continue;
+        }
+        println!("ip: {}, netmask: {}", &caps["ip"], &caps["netmask"]);
+    }
+    let re_mac = Regex::new(r"ether (?P<mac>[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2}:[[:xdigit:]]{2})").unwrap();
+    for caps in re_mac.captures_iter(text) {
+        println!("mac: {}", &caps["mac"]);
+    }
 }
